3f19e61b8c63e30e3f500a4202196aed65b4bf6b
Alexander Yfraimov <yfraimoa@flip1.engr.oregonstate.edu>
2018-10-13 09:09:54 -0700

    Changed dominion.h second time

--- projects/yfraimoa/dominion/dominion.h
+++ projects/yfraimoa/dominion/dominion.h
@@ -128,6 +128,14 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int cardGardens();
+int cardVillage(struct gameState * state, int handPos, int currentPlayer);
+
+int cardSmithy (struct gameState * state, int handPos, int currentPlayer);
+
+int cardAdventurer (struct gameState * state);
+
+int cardGreatHall (struct gameState * state, int handPos, int currentPlayer);
+
+int carOutpost (struct gameState * state, int handPos, int currentPlayer);
 
 #endif
