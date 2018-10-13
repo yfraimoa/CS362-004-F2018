67d04b00e9c59b0ad3bc78f52e05c818dfde3fd0
Alexander Yfraimov <yfraimoa@flip1.engr.oregonstate.edu>
2018-10-13 07:43:50 -0700

    Changed dominion.h first time

--- projects/yfraimoa/dominion/dominion.h
+++ projects/yfraimoa/dominion/dominion.h
@@ -128,4 +128,6 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int cardGardens();
+
 #endif
