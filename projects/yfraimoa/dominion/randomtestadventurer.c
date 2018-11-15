5a23f6343b86e3360268ebc36826e618ec9667c9
Alexander Yfraimov <yfraimoa@flip1.engr.oregonstate.edu>
2018-11-14 18:03:27 -0800

    added rta.c

---
+++ projects/yfraimoa/dominion/randomtestadventurer.c
@@ -0,0 +1,30 @@
+#include "dominion.h"
+#include <time.h>
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+
+int main() {
+	printf("Test Adventurer");
+	struct gameState g1, g2;
+	time_t t;
+	srand((unsigned) time(&t));
+  	
+	int* k = kingdomCards(adventurer, gardens, village, embargo, minion, mine, cutpurse, tribute, sea_hag, smithy);
+	int i = 0;
+ 
+	for (i = 0; i < 1000; i++){
+		initializeGame(2,k,rand()%5000,&g1); 
+		memcpy(&g2,&g1,sizeof(struct gameState));
+		cardAdventurer(&g1);
+	}
+
+//	printf("Should display: %d cards, Our result is: %d cards\n", g2.handCount[g2.whoseTurn] + 1, g1.handCount[g1.whoseTurn]);
+	
+	return 0;
+}
+
